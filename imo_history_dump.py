e9d1e664ffcea1fc98ca6a35eccf077320cf4619
Dean Camera <dean@fourwalledcubicle.com>
2013-07-01 20:12:22 +0200

    Add information header.

--- imo_history_dump.py
+++ imo_history_dump.py
@@ -1,3 +1,39 @@
+'''
+			IMO.im JSON history to linear log converter.
+
+	By Dean Camera (dean [at] fourwalledcubicle [dot] com)
+	Released into the public domain.
+
+	This script converts a IMO.im JSON log into a linear log for a given user
+	email address.
+
+	Sample input:
+
+		{
+		  "user@example.com": [
+		    {
+		      "from": "me",
+		      "message": "Test message from me to Example User",
+		      "timestamp": "2012-11-26 07:21:45",
+		      "to": "user@example.com"
+		    },
+		    {
+		      "from": "user@example.com",
+		      "message": "Test message from Example User to me",
+		      "timestamp": "2012-11-26 07:22:57",
+		      "to": "me"
+		    }
+		}
+
+	Sample output:
+
+		Chat history for 'user@example.com':
+		=====================================
+		2012-11-26 07:21:45    Me      Test message from me to Example User
+		2012-11-26 07:22:57    Them    Test message from Example User to me
+		=====================================
+'''
+
 import json
 import sys
 
